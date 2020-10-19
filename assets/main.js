4cb833ca23d001b922a54f0e681d478d4eda5cbb
Marco Guidi <mark.88g@gmail.com>
2020-10-19 18:33:03 +0200

    aggiunta comunicazione all'utente

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,11 @@
 // <1-fold --- VERSION 1.0 ---
 // VAR ASSIGNMENT
-var lastNameList = ["Bianchi", "Verdi", "Rossi", "Esposito", "Conte"];
+var lastNameList = ["bianchi", "verdi", "rossi", "esposito", "conte", "antognoni"];
 var usersLastName;
 var usersLastNameIndex;
 
 // ASK NAME AND PUSH INTO THE LIST
-usersLastName = prompt("Inserisci il tuo cognome:");
+usersLastName = prompt("Inserisci il tuo cognome:").toLowerCase();
 lastNameList.push(usersLastName);
 
 // CHECK
@@ -17,9 +17,9 @@ lastNameList.sort();
 // STAMP THE INDEX
 usersLastNameIndex = lastNameList.indexOf(usersLastName) + 1;
 
-// CHECK
+// CHECK - STAMP - STAMP HTML
 console.log(lastNameList);
 console.log("Il tuo cognome è il numero " + usersLastNameIndex + " della lista.");
-
+document.getElementById("users-last-name-index").innerHTML = usersLastNameIndex;
 
 // </1-fold
