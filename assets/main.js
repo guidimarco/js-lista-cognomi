c84568eada85365c2faf9e987395f72361d5d98d
Marco Guidi <mark.88g@gmail.com>
2020-10-19 17:42:32 +0200

    prompt richiesta cognome e inserimento in lista

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+// <1-fold --- VERSION 1.0 ---
+// VAR ASSIGNMENT
+var lastNameList = ["Bianchi", "Verdi", "Rossi", "Esposito", "Conte"];
+var usersLastName;
+
+// CHECK
+console.log(lastNameList);
+
+// ASK NAME AND PUSH INTO THE LIST
+usersLastName = prompt("Inserisci il tuo cognome:");
+lastNameList.push(usersLastName);
+
+// CHECK
+console.log("Cognome utente: " + usersLastName);
+console.log(lastNameList);
+
+
+
+// </1-fold
